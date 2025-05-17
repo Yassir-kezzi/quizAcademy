638e41bae699e374a3bd1a407dcfb761fd7fa115
YASSIR KEZZI <kezziyassir005@gmail.com>
2025-05-17 18:24:04 +0100

    test

--- connexion.php
+++ connexion.php
@@ -5,3 +5,22 @@ $username = 'root';
 $password = '';          
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#nasnasdnasd
